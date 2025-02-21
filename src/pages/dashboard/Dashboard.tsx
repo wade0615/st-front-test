47ff84f6995067790fc7732264f193f5eeceb3e3
wsw0615 <wsw0615@gmail.com>
2025-02-21 11:16:13 +0800

    feat: getTaiwanStockMonthRevenueList

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,20 +1,76 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { InitState } from 'store/reducers/initReducer';
 import 'pages/dashboard/Dashboard.scss';
 
+import api from 'services/api';
+
 import Chart from './chart/Chart';
 import Table from './table/Table';
 
+import ExceptionHandleService from 'utils/ExceptionHandler';
+
+const _EHS = new ExceptionHandleService({
+  _NAME: 'pages/dashboard/Dashboard.tsx',
+  _NOTICE: ''
+});
+
 function Dashboard() {
   const [curTarget, setCurTarget] = useState<string>('');
+  const [taiwanStockInfoList, setTaiwanStockInfoList] = useState<
+    {
+      xAxis: string;
+      yAxis_1: number;
+      yAxis_2: number;
+    }[]
+  >([]);
+
   const selectedValue = useSelector(
     (state: { init: InitState }) => state.init.selectedValue
   );
 
+  /** 取得列表 */
+  const getTaiwanStockMonthRevenueList = useCallback(async () => {
+    try {
+      const list = await api.getTaiwanStockMonthRevenueList();
+      if (list) {
+        return list.map((item) => ({
+          xAxis: `${item.revenue_year}${item.revenue_month}`,
+          yAxis_1: item.revenue,
+          yAxis_2:
+            item.revenue /
+            parseInt(`${item.revenue_year}${item.revenue_month}`, 10)
+        }));
+      }
+    } catch (error) {
+      const errorMessage = (error as Error).message;
+      _EHS.errorReport(
+        errorMessage,
+        'getTaiwanStockMonthRevenueList',
+        _EHS._LEVEL.ERROR
+      );
+    }
+  }, []);
+
+  // // 初始化
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const stockList = await getTaiwanStockMonthRevenueList();
+        if (stockList) {
+          setTaiwanStockInfoList(stockList);
+        }
+      } catch (error) {
+        const errorMessage = (error as Error).message;
+        _EHS.errorReport(errorMessage, 'init', _EHS._LEVEL.ERROR);
+      }
+    };
+    init();
+  }, [getTaiwanStockMonthRevenueList]);
+
   useEffect(() => {
     setCurTarget(selectedValue || '');
-  }, [selectedValue]);
+  }, [setCurTarget, selectedValue]);
 
   const generateFakeData = () => {
     const data = [];
@@ -40,12 +96,12 @@ function Dashboard() {
         </div>
         <div className='chart-container'>
           <p>數據圖表</p>
-          <Chart chartData={generateFakeData()} />
+          <Chart chartData={taiwanStockInfoList || generateFakeData()} />
         </div>
       </section>
       <section>
         <button>詳細數據</button>
-        <Table tableData={generateFakeData()} />
+        <Table tableData={taiwanStockInfoList || generateFakeData()} />
       </section>
       <footer>
         <p>圖表單位：千元，數據來自公開資訊官測站</p>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,7 @@
-import { get } from './sub_services/base';
+import TaiwanStockInfo from 'fakeData/TaiwanStockInfo.json';
+import TaiwanStockMonthRevenue from 'fakeData/TaiwanStockMonthRevenue.json';
+
+// import { get } from './sub_services/base';
 import ExceptionHandleService from 'utils/ExceptionHandler';
 
 const _EHS = new ExceptionHandleService({
@@ -6,9 +9,9 @@ const _EHS = new ExceptionHandleService({
   _NOTICE: ''
 });
 
-const baseUrl = 'https://api.finmindtrade.com/api/v4/data';
-const token =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyNS0wMi0yMCAxOTo0MjowNCIsInVzZXJfaWQiOiJ3c3cwNjE1IiwiaXAiOiIyMTkuODUuMzAuMjAzIn0.wPp17AOBz-lfa3B8yYhtmcfOYMYJnk7SVXOPY3CE6sA';
+// const baseUrl = 'https://api.finmindtrade.com/api/v4/data';
+// const token =
+//   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyNS0wMi0yMCAxOTo0MjowNCIsInVzZXJfaWQiOiJ3c3cwNjE1IiwiaXAiOiIyMTkuODUuMzAuMjAzIn0.wPp17AOBz-lfa3B8yYhtmcfOYMYJnk7SVXOPY3CE6sA';
 
 const getTaiwanStockInfoList = async (): Promise<
   {
@@ -20,8 +23,9 @@ const getTaiwanStockInfoList = async (): Promise<
   }[]
 > => {
   try {
-    const url = `${baseUrl}?dataset=TaiwanStockInfo&token=${token}`;
-    const result = await get(url);
+    // const url = `${baseUrl}?dataset=TaiwanStockInfo&token=${token}`;
+    // const result = await get(url);
+    const result = TaiwanStockInfo;
     return result;
   } catch (error) {
     const errorMessage = (error as Error).message;
@@ -30,4 +34,35 @@ const getTaiwanStockInfoList = async (): Promise<
   }
 };
 
-export { getTaiwanStockInfoList };
+const getTaiwanStockMonthRevenueList = async (): Promise<
+  {
+    date: string;
+    stock_id: string;
+    country: string;
+    revenue: number;
+    revenue_month: number;
+    revenue_year: number;
+  }[]
+> => {
+  try {
+    // const url = `${baseUrl}?dataset=TaiwanStockInfo&token=${token}&data_id=2330&start_date=2024-01-01&end_date=2025-02-28`;
+    // const result = await get(url);
+    const result = TaiwanStockMonthRevenue;
+    return result;
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+    _EHS.errorReport(
+      errorMessage,
+      'getTaiwanStockMonthRevenueList',
+      _EHS._LEVEL.ERROR
+    );
+    return Promise.reject(errorMessage);
+  }
+};
+
+const api = {
+  getTaiwanStockInfoList,
+  getTaiwanStockMonthRevenueList
+};
+
+export default api;
